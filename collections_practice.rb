d1c03c56b246228b7d2a824e8456accd98664ce6
Jason Leach <jlspursfan@gmail.com>
2019-05-26 17:16:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,5 @@
 # your code goes here
 def begins_with_r(tools)
-  tools.each do |ele|
-    if ele.start_with?("r")
-      return true
-    else
-       return false
-    end
-  end
+  tools.all? { |ele| ele.start_with?("r") }
+      
 end
